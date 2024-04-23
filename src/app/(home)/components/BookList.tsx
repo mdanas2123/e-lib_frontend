d355797d3a370568b9eefc7d2e6181b07ce75da4
mdanas2123 <22bca0239@gmail.com>
2024-04-24 00:49:27 +0530

    create book card

---
+++ src/app/(home)/components/BookList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Book } from '@/types'
+import BookCard from './BookCard'
+const BookList = ({books}:{books:Book[]}) => {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
+    {books.map((book: Book) => (
+        <BookCard key={book._id} book={book} />
+    ))}
+</div>
+   
+  )
+}
+
+export default BookList
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,22 @@
 import Banner from "@/app/(home)/components/Banner";
+import BookList from "./components/BookList";
 
-export default function Home() {
-  return (
-    <>
-    <Banner/>
-    </>
-  );
+export default async function Home() {
+  try {
+    const response = await fetch(`${process.env.Backend_URL}/books`);
+    if (!response.ok) {
+      throw new Error('Error occurred while fetching books');
+    }
+    const books = await response.json();
+    console.log("books", books);
+    return (
+      <>
+        <Banner />
+        <BookList books={books}/>
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    // You might want to handle the error here, e.g., show an error message to the user
+  }
 }
