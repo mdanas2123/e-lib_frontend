d0fdf60e5cfa356b81deef8900d3279141c37013
mdanas2123 <22bca0239@gmail.com>
2024-08-24 19:25:37 +0530

    production setup

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import BookCard from './BookCard';
-import { Book } from '@/types';
+import React from "react";
+import BookCard from "./BookCard";
+import { Book } from "@/types";
 
 const BookList = async () => {
-    // data fetching
-    const response = await fetch(`${process.env.BACKEND_URL}/books`, { cache: 'no-store' });
-    if (!response.ok) {
-        throw new Error('An error occurred while fetching the books');
-    }
+  // data fetching
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, {
+    cache: "no-store",
+  });
 
-    const books = await response.json();
+  if (!response.ok) {
+    throw new Error("An error occurred while fetching the books");
+  }
 
-    return (
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
-            {books.map((book: Book) => (
-                <BookCard key={book._id} book={book} />
-            ))}
-        </div>
-    );
+  const books = await response.json();
+
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
+      {books.map((book: Book) => (
+        <BookCard key={book._id} book={book} />
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
